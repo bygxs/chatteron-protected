9d4679a19cb6929c11c73deee9e62875c0f32864
bygxs <biniyamyohannes@icloud.com>
2025-03-10 21:44:03 +0100

    welcome, {user.email} added}

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,18 +1,23 @@
 // src/app/dashboard/page.tsx
 "use client";
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import { auth } from "../lib/firebase";
 import Navbar from "../components/Navbar";
 
 export default function Dashboard() {
+  const [userEmail, setUserEmail] = useState<string | null>(null);
   const router = useRouter();
 
   useEffect(() => {
+    // Redirect if user is not authenticated
     const unsubscribe = auth.onAuthStateChanged((user) => {
       if (!user) {
         router.push("/signin-form");
+      } else {
+        // Set the user's email
+        setUserEmail(user.email);
       }
     });
 
@@ -21,10 +26,10 @@ export default function Dashboard() {
 
   return (
     <div className="min-h-screen bg-gray-100 dark:bg-gray-900">
-   {/*    <Navbar /> */}
+    
       <div className="flex flex-col items-center justify-center pt-20">
         <h1 className="text-4xl font-bold text-gray-800 dark:text-gray-200 mb-4">
-          Welcome to the Dashboard
+          Welcome, {userEmail || "User"}, to the Dashboard
         </h1>
         <p className="text-gray-600 dark:text-gray-400">
           This is a protected dashboard page.
