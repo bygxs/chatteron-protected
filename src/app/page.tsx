5116405a7f0120826d81a541f32e39769f554709
bygxs <biniyamyohannes@icloud.com>
2025-03-11 14:39:27 +0100

    global.css light/dark mode color changed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Navbar from "../app/components/Navbar";
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-gray-100 dark:bg-gray-900">
+    <div className="min-h-screen">
       <div className="flex flex-col items-center justify-center pt-20">
         <h1 className="text-4xl font-bold text-gray-800 dark:text-gray-200 mb-4">
           Welcome to the Landing Page
